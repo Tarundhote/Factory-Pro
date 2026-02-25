0a84589d0d6d5fa77836b0230a7c1b3728541383
Tarun Dhote <tarundhote@example.com>
2026-02-25 17:09:02 +0530

    changes

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const navLinks = document.querySelector(".nav-links");
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("sticky", window.scrollY > 50);
+});
